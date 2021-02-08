78b08fd2c9149b0d1c89c744c59339efafe71d05
Priscila <sacchipriscila@gmail.com>
2021-02-08 19:34:30 -0300

    asd

--- hangman_api.py
+++ hangman_api.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import random
 import re
 from functools import wraps, update_wrapper
-from datetime import datetime
+from datetime import datetime 
 import os
 
 # Create our flask app
