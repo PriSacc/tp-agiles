1f9c9f5f71d3c1c9c8873d7d1e190948292afba0
Priscila <sacchipriscila@gmail.com>
2021-02-08 18:28:38 -0300

    cambios

--- hangman_api.py
+++ hangman_api.py
@@ -60,11 +60,11 @@ def reset_session():
     session['correct_chars'] = [] # Correct guesses
     session['gameStatus'] = 0 # -1,0 or 1 for lost, in progress or won
 
-@app.route('/')
+@app.route('/') 
 @app.errorhandler(404)
 def not_found(e):
     return app.send_static_file('index.html')
-    
+
 def index():
     return app.send_static_file('index.html')
 
